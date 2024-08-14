53cec046391022442422a261c249700602b94cc2
Pakorn <>
2024-08-14 15:32:32 +0700

    update 0.0.15

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
-
+import terser from "@rollup/plugin-terser";
 const packageJson = require("./package.json");
 import postcss from "rollup-plugin-postcss";
 
@@ -23,6 +23,7 @@ export default [
       },
     ],
     plugins: [
+      terser(),
       peerDepsExternal(),
       resolve(),
       commonjs(),
