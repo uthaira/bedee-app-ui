2119e3a9fe3c15522eb93cc1fc4dc50ffed013c9
Pakorn <>
2024-08-14 15:21:08 +0700

    update version 0.0.14

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import terser from "@rollup/plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 const packageJson = require("./package.json");
@@ -28,10 +27,9 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      terser(),
       postcss(), 
     ],
-    external: ["react", "react-dom", "@emotion/react", "@emotion/styled", "@mui/material"],
+    external: ["react", "react-dom", "@emotion/react", "@emotion/styled", "@mui/material","@mui/icons-material"],
   },
   {
     input: "src/index.ts",
