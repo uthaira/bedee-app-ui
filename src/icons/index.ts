e5a7bcca0e4814021a7e99bd8701e261c79d276c
tammanoon.jo <tammanoon.jo@health-plaza.com>
2024-08-20 15:02:56 +0700

    add: export Icon

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,8 +1,35 @@
-import LogoIcon from './Logo';
-import CircleIcon from './Circle';
-import DelIcon from './Del';
-import RemoveIcon from './Remove';
-import CloseIcon from './CloseIcon';
-import CheckIcon from './Check';
+import LogoIcon from "./Logo";
+import CircleIcon from "./Circle";
+import DelIcon from "./Del";
+import RemoveIcon from "./Remove";
+import CloseIcon from "./CloseIcon";
+import CheckIcon from "./Check";
+import CalendarIcon from "./calendar";
+import DocumentTextIcon from "./document-text";
+import EditIcon from "./edit";
+import UserIcon from "./user";
+import Globe from "./globe";
+import Identification from "./identification";
+import PhoneIcon from "./phone";
+import ShieldCheckIcon from "./shield-check";
+import SpeakerphoneIcon from "./speakerphone";
+import TrashbinIcon from "./trashbin";
 
-export { LogoIcon, CircleIcon, DelIcon, RemoveIcon, CloseIcon,CheckIcon };
+export {
+  LogoIcon,
+  CircleIcon,
+  DelIcon,
+  RemoveIcon,
+  CloseIcon,
+  CheckIcon,
+  CalendarIcon,
+  DocumentTextIcon,
+  EditIcon,
+  UserIcon,
+  Globe,
+  Identification,
+  PhoneIcon,
+  ShieldCheckIcon,
+  SpeakerphoneIcon,
+  TrashbinIcon,
+};
