d80519896ff0ef3eb860e374a26d1cd000447af1
tammanoon.jo <tammanoon.jo@health-plaza.com>
2024-08-30 16:41:31 +0700

    fix: dob picker with clear icon

--- src/components/input/DOBPickerInput.tsx
+++ src/components/input/DOBPickerInput.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, IconButton, InputAdornment, Typography } from "@mui/material";
 import {
+  ClearIcon,
   DatePicker,
   DatePickerProps,
+  PickersActionBar,
   PickerValidDate,
 } from "@mui/x-date-pickers";
 import { SxProps, Theme } from "@mui/material/styles";
@@ -28,12 +30,34 @@ const DOBPicker: React.FC<DOBPickerProps> = ({
   backgroundColor,
   disabled,
   error,
+  value,
+  defaultValue,
+  onChange,
   ...props
 }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedDate, handleDateChange] = useState<PickerValidDate | null>(defaultValue || null);
 
   const handleOpen = () => setIsOpen(true);
   const handleClose = () => setIsOpen(false);
+
+  const handleClear = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation(); // Prevent the DatePicker from opening
+    handleDateChange(null);
+  };
+
+  const onDateChange = (date: PickerValidDate | null) => {
+    handleDateChange(date);
+    if (onChange) {
+      onChange(date, { validationError: null });
+    }
+  };
+
+  useEffect(() => {
+    if (value) {
+      handleDateChange(value);
+    }
+  }, [value]);
 
   return (
     <Box sx={sx}>
@@ -53,6 +77,8 @@ const DOBPicker: React.FC<DOBPickerProps> = ({
         open={isOpen}
         onOpen={handleOpen}
         onClose={handleClose}
+        value={selectedDate}
+        onChange={onDateChange}
         slots={{
           textField: (props) => (
             <TextInput
@@ -65,6 +91,16 @@ const DOBPicker: React.FC<DOBPickerProps> = ({
               InputProps={{
                 endAdornment: (
                   <InputAdornment position="end">
+                    {props.value && (
+                      <IconButton
+                        edge="end"
+                        onClick={handleClear}
+                        disabled={disabled}
+                        size="small"
+                      >
+                        <ClearIcon fontSize="small" />
+                      </IconButton>
+                    )}
                     <IconButton 
                       edge="end" 
                       onClick={handleOpen}
