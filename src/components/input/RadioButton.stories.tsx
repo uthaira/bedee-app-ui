05138943bd2de7764acef3338f2093c10e358494
Tammanoon max <tammanoon.jo@health-plaza.com>
2024-09-05 11:11:15 +0700

    feat: radio button

---
+++ src/components/input/RadioButton.stories.tsx
@@ -0,0 +1,38 @@
+import RadioButton from "./RadioButton";
+import { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: 'Components/RadioButton',
+  component: RadioButton,
+  argTypes: {
+    value: {
+      control: { type: 'boolean' },
+      defaultValue: false,
+    },
+    message: {
+      control: { type: 'text' },
+      defaultValue: 'This is a radio button',
+    },
+    disabled: {
+      control: { type: 'boolean' },
+      defaultValue: false,
+    },
+  },
+} as Meta<typeof RadioButton>
+
+type Story = StoryObj<typeof RadioButton>;
+export const Default: Story = {
+
+}
+
+export const Checked: Story = {
+  args: {
+    value: true
+  }
+}
+
+export const Disabled: Story = {
+  args: {
+    disabled: true
+  }
+}
