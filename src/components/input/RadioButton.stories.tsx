db1099ad0af21aecd581f184e0179e257e89cdf7
Tammanoon max <tammanoon.jo@health-plaza.com>
2024-09-19 14:10:57 +0700

    fix: can custom sx style radio button

--- src/components/input/RadioButton.stories.tsx
+++ src/components/input/RadioButton.stories.tsx
@@ -1,3 +1,4 @@
+import { Colors } from "../../colors";
 import RadioButton from "./RadioButton";
 import { Meta, StoryObj } from "@storybook/react";
 
@@ -28,6 +29,15 @@ export const Default: Story = {
 export const Checked: Story = {
   args: {
     value: true
+  }
+}
+
+export const customColor: Story = {
+  args: {
+    unCheckStyle: {
+      border: "1px solid rgba(195, 202, 212, 1)",
+      backgroundColor: Colors.white
+    }
   }
 }
 
