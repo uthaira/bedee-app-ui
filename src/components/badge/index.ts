b92f6e5f978db699bc9e65191a2265c30a443a58
Pakorn <>
2024-09-26 10:37:59 +0700

    feat: provider badge

--- src/components/badge/index.ts
+++ src/components/badge/index.ts
@@ -1,2 +1,4 @@
-export { default as Badge } from './Badge'
-export type {BadgeProps } from './Badge'
+export { default as Badge } from "./Badge";
+export type { BadgeProps } from "./Badge";
+export { default as ProviderBadge } from "./ProviderBadge";
+export type { ProviderBadgeProps } from "./ProviderBadge";
