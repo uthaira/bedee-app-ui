b14c803109b52d0ad37f493dbc240f532f6bd418
Bank Aekkawan <aekkawan.k@gmail.com>
2024-10-07 11:59:33 +0700

    feat: add address icons

---
+++ src/icons/address/MarkerIcon.tsx
@@ -0,0 +1,107 @@
+export default function MarkerIcon() {
+  return (
+    <svg
+      width='58'
+      height='68'
+      viewBox='0 0 58 68'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <g filter='url(#filter0_d_4944_234687)'>
+        <g filter='url(#filter1_d_4944_234687)'>
+          <path
+            fillRule='evenodd'
+            clipRule='evenodd'
+            d='M30.758 61.2873C36.3714 55.9552 53.5 38.7343 53.5 27.1298C53.5 13.251 42.531 2 29 2C15.469 2 4.5 13.251 4.5 27.1298C4.5 38.7343 21.6286 55.9552 27.242 61.2873C28.2424 62.2376 29.7576 62.2376 30.758 61.2873ZM29 37.9986C35.0138 37.9986 39.8889 33.0395 39.8889 26.9221C39.8889 20.8047 35.0138 15.8456 29 15.8456C22.9862 15.8456 18.1111 20.8047 18.1111 26.9221C18.1111 33.0395 22.9862 37.9986 29 37.9986Z'
+            fill='url(#paint0_linear_4944_234687)'
+          />
+        </g>
+        <circle cx='29' cy='26.5' r='11.5' fill='white' />
+      </g>
+      <defs>
+        <filter
+          id='filter0_d_4944_234687'
+          x='0.5'
+          y='0'
+          width='57'
+          height='68'
+          filterUnits='userSpaceOnUse'
+          colorInterpolationFilters='sRGB'
+        >
+          <feFlood floodOpacity='0' result='BackgroundImageFix' />
+          <feColorMatrix
+            in='SourceAlpha'
+            type='matrix'
+            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+            result='hardAlpha'
+          />
+          <feOffset dy='2' />
+          <feGaussianBlur stdDeviation='2' />
+          <feComposite in2='hardAlpha' operator='out' />
+          <feColorMatrix
+            type='matrix'
+            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0'
+          />
+          <feBlend
+            mode='normal'
+            in2='BackgroundImageFix'
+            result='effect1_dropShadow_4944_234687'
+          />
+          <feBlend
+            mode='normal'
+            in='SourceGraphic'
+            in2='effect1_dropShadow_4944_234687'
+            result='shape'
+          />
+        </filter>
+        <filter
+          id='filter1_d_4944_234687'
+          x='0.5'
+          y='0'
+          width='57'
+          height='68'
+          filterUnits='userSpaceOnUse'
+          colorInterpolationFilters='sRGB'
+        >
+          <feFlood floodOpacity='0' result='BackgroundImageFix' />
+          <feColorMatrix
+            in='SourceAlpha'
+            type='matrix'
+            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+            result='hardAlpha'
+          />
+          <feOffset dy='2' />
+          <feGaussianBlur stdDeviation='2' />
+          <feComposite in2='hardAlpha' operator='out' />
+          <feColorMatrix
+            type='matrix'
+            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0'
+          />
+          <feBlend
+            mode='normal'
+            in2='BackgroundImageFix'
+            result='effect1_dropShadow_4944_234687'
+          />
+          <feBlend
+            mode='normal'
+            in='SourceGraphic'
+            in2='effect1_dropShadow_4944_234687'
+            result='shape'
+          />
+        </filter>
+        <linearGradient
+          id='paint0_linear_4944_234687'
+          x1='39.9828'
+          y1='46.1176'
+          x2='-9.86509'
+          y2='22.0649'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#2F54B6' />
+          <stop offset='0.372197' stopColor='#3384E6' />
+          <stop offset='0.798591' stopColor='#59A2FF' />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
