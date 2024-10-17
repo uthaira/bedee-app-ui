de9c0b7b3248858a90df1d51898363f634984ed9
Paveena Luerkajon <paveena.lu@bdms.co.th>
2024-10-17 17:12:31 +0700

    update to 0.1.46

--- src/language/languageContext.tsx
+++ src/language/languageContext.tsx
@@ -29,20 +29,25 @@ export const LanguageProvider: React.FC<{
     Cookie.setCookie('i18next', language, { path: '/' });
   };
 
-  useEffect(() => {
-    const savedLang = Cookie.getCookie('i18next');
-    if (savedLang && savedLang !== lang && i18n && i18n?.changeLanguage) {
-      i18n.changeLanguage(savedLang);
-    }
-  }, [lang, i18n]);
+  // useEffect(() => {
+  //   const savedLang = Cookie.getCookie('i18next');
+  //   if (savedLang && savedLang !== lang && i18n && i18n?.changeLanguage) {
+  //     i18n.changeLanguage(savedLang);
+  //   }
+  // }, [lang, i18n]);
 
   useEffect(() => {
     if (userInfo && userInfo?.preferredLanguage) {
       if (lang !== userInfo?.preferredLanguage) {
         changeLanguage(userInfo?.preferredLanguage);
       }
+    } else {
+      const savedLang = Cookie.getCookie('i18next');
+      if (savedLang && savedLang !== lang && i18n && i18n?.changeLanguage) {
+        i18n.changeLanguage(savedLang);
+      }
     }
-  }, [userInfo])
+  }, [userInfo, lang, i18n])
 
   return (
     <LanguageContext.Provider value={{ lang, changeLanguage }}>
