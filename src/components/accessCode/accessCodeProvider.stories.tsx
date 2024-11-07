735f41572264d1c242fc0f6755e9f59b04193a67
Tanakorn Paorat <tanakorn.pa@bdms.co.th>
2024-11-07 12:18:00 +0700

    feat: improve access code storybook

--- src/components/accessCode/accessCodeProvider.stories.tsx
+++ src/components/accessCode/accessCodeProvider.stories.tsx
@@ -1,19 +1,28 @@
-import { Meta, StoryObj } from '@storybook/react'
-import AccessCode from './accessCodeProvider'
+import { Meta } from '@storybook/react'
+import { AccessCodeProvider, Cookie } from '../..'
+import { useState } from 'react'
+
+export const AccessCodeProviderSample = () => {
+  const [rerender, setRerender] = useState(false)
+
+  return (
+    <AccessCodeProvider domain="http://localhost:3000" cookieKey="cookieKey" accessCode="password" cookieValue="true">
+      <div>
+        <div>You have accessed the site</div>
+        <button
+          onClick={() => {
+            Cookie.removeCookie('cookieKey')
+            setRerender(!rerender)
+          }}
+        >
+          Clear cookie
+        </button>
+      </div>
+    </AccessCodeProvider>
+  )
+}
 
 export default {
-  title: 'Components/accessCode/accessCodeProvider',
-  component: AccessCode,
-} as Meta<typeof AccessCodeProvider>
-
-type AccessCodeProviderStory = StoryObj<typeof AccessCode>
-
-export const AccessCodeProvider: AccessCodeProviderStory = {
-  args: {
-    domain: 'http://localhost:3000',
-    cookieKey: 'cookieKey',
-    accessCode: 'test',
-    cookieValue: 'true',
-    children: <div>You have accessed the site</div>,
-  },
-}
+  title: 'Components/AccessCodeProvider',
+  component: AccessCodeProviderSample,
+} as Meta<typeof AccessCodeProviderSample>
